524f30708e06fce442a749e8488f235aed36b243
hhertout <hugues.hertout@gmail.com>
2024-06-09 18:07:22 +0200

    feat: adding get one by id & delete

--- src/controller/item_controller.py
+++ src/controller/item_controller.py
@@ -17,12 +17,33 @@ async def get_all_items(item_service: ItemService = Depends(ItemService)):
         print(e)
         return {"error": str(e)}
 
+@router.get("/{item_id}")
+async def get_item_by_id(item_id: int, item_service: ItemService = Depends(ItemService)):
+    try:
+        item = await item_service.get_item_by_id(item_id)
+        return item
+    except Exception as e:
+        print(e)
+        return {"error": str(e)}
+
 @router.post("/")
 async def create_item(item: ItemBody, item_service: ItemService = Depends(ItemService)):
     print(item)
     try: 
         created_item = await item_service.create_item(Items(name=item.name))
         return created_item
+    except Exception as e:
+        print(e)
+        return {"error": str(e)}
+
+@router.delete("/{item_id}")
+async def delete_item(item_id: int, item_service: ItemService = Depends(ItemService)):
+    try:
+        res = await item_service.delete_item(item_id)
+        if res is True:
+            return {"message": "Item deleted"}
+        else:
+            return {"message": "An error has occurred. Item not deleted."}
     except Exception as e:
         print(e)
         return {"error": str(e)}
--- src/services/item_service.py
+++ src/services/item_service.py
@@ -4,14 +4,14 @@ from src.db import get_db
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from sqlalchemy import insert
+from sqlalchemy import insert, delete
 from typing import List
 from src.models.item_model import Items
 
 class ItemService:
     def __init__(self, db: AsyncSession = Depends(get_db)):
         self.db = db
-    
+
     async def get_all_items(self) -> List[Items]:
         async with self.db as session:
             result = await session.execute(
@@ -19,10 +19,29 @@ class ItemService:
             )
             items = result.scalars().all()
         return items
-    
+
+    async def get_item_by_id(self, item_id: int) -> Items or None:
+        async with self.db as session:
+            result = await session.execute(
+                select(Items).where(Items.id == item_id)
+            )
+            item = result.scalars().first()
+        return item
+
     async def create_item(self, item: Items) -> Items:
         db_item = Items(name=item.name)
         async with self.db.begin():
             self.db.add(db_item)
             await self.db.commit()
         return db_item
+
+    async def delete_item(self, item_id: int) -> bool:
+        item = await self.get_item_by_id(item_id)
+        if item is None: 
+            return False
+        
+        async with self.db as session:
+            query = delete(Items).where(Items.id == item_id)
+            await session.execute(query)
+            await session.commit()
+            return True
